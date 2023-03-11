012e47a12b1a42c0cd1a6d65d13b03416aa03277
zmx777zmx <98063800+zmx777zmx@users.noreply.github.com>
2023-03-11 14:42:09 +0800

    Add files via upload

---
+++ gamescene/sceneContainer.gd
@@ -0,0 +1,7 @@
+extends VBoxContainer
+
+#var gamescene = preload("res://LevelSelection_level1.tscn")
+
+#func _on_TextureButton_focus_entered():
+#	$TextureButton.texture_normal = gamescene
+#	pass # Replace with function body.
